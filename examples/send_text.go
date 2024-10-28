20b2a1c614ff5508f87497cf5107d3160d01bd2c
Allen Lin <allenlinsh@gmail.com>
2024-10-28 16:37:39 -0400

    add godotenv for example

--- examples/send_text.go
+++ examples/send_text.go
@@ -3,11 +3,18 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/gtfol/textfully-go"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load .env file from the same directory as the executable
+	if err := godotenv.Load(filepath.Join(".", ".env")); err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
 	apiKey := os.Getenv("TEXTFULLY_API_KEY")
 	if apiKey == "" {
 		log.Fatal("TEXTFULLY_API_KEY environment variable not set")
